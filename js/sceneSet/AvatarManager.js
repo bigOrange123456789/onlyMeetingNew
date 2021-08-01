0192fe0de9267ae05fa3c766aefa601f391150ea
bigOrange123456789 <2814255951@qq.com>
2021-08-01 22:04:16 +0800

    SAVE

--- js/sceneSet/AvatarManager.js
+++ js/sceneSet/AvatarManager.js
@@ -181,34 +181,26 @@ function AvatarManager(mySeatManager,camera){//camera用于LOD
     this.host=function () {
         var scope=this;
         let order_list=[
-            [1, 2, 3, 3, 1, 2, 1, 2, 1, 2, 1, 2, 1, 3, 1, 3, 1, 4, 3, 3]
+            [1, 2, 3, 3, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0, 3, 1, 2, 1, 4]
         ];
-        let time_list=[2736];
+        let time_list=[3845];
+        var move_num = order_list[0].length;//面部动作数量
+        var move_time = time_list[0]/move_num;//每个动作的时间
         //var model_url = "./voice.gltf";
         var model_url = "./myModel/host/voice2.glb";//"../_DATA_/voice.glb";
-        var mp3_url = "./myModel/host/voice.mp3";//"../_DATA_/voice.mp3";
+        var mp3_url = "./myModel/host/vioce.m4a";//"../_DATA_/voice.mp3";
         var mp3 = new Audio(mp3_url);
 
         var mixer = {};
         var clock = new THREE.Clock();
         var upda = 0;
-        var past_time = -1000;//第一次播放时，音频会相对动画延迟。
-        function timeLine(){
-            var time_line = [];
-            time_line.push(0);
-            for(let i = 1; i < order_list.length; i++){
-                var time = time_line[i-1] + 500 +time_list[i-1];
-                time_line.push(time);
-            }
-            return time_line;
-        }
-        var time_line = timeLine();
-        console.log("time_line",time_line)
+        var past_time =0;//第一次播放时，音频会相对动画延迟。
 
         initModel()
         function initModel(){//ResourceLoader.js中加载人物模型（function initModel）
             var loader=new THREE.GLTFLoader();
             loader.load(model_url, function (obj){
+                console.log(obj)
                 var model = obj.scene;
                 //console.log(model);
                 model.position.set(198,9,-65);
@@ -217,74 +209,74 @@ function AvatarManager(mySeatManager,camera){//camera用于LOD
                 model.traverse(function (node) {
                     if(node instanceof THREE.SkinnedMesh){
                         node.material.side=THREE.DoubleSide;
-                        //alert(node.frustumCulled)
                         node.frustumCulled=false;
                     }
                 })
-                console.log(model)
                 scope.obj.add(model);
                 //将模型绑定到动画混合器里面
                 var anima = obj.animations;
-                mixer[0] = new THREE.AnimationMixer( model );
-                mixer[1] = new THREE.AnimationMixer( model );
-                mixer[2] = new THREE.AnimationMixer( model );
-                mixer[3] = new THREE.AnimationMixer( model );
-                mixer[4] = new THREE.AnimationMixer( model );
-                mixer[5] = new THREE.AnimationMixer( model );
+                mixer[0] = new Pack.AnimationMixer( model );
+                mixer[1] = new Pack.AnimationMixer( model );
+                mixer[2] = new Pack.AnimationMixer( model );
+                mixer[3] = new Pack.AnimationMixer( model );
+                mixer[4] = new Pack.AnimationMixer( model );
+                mixer[5] = new Pack.AnimationMixer( model );
+                mixer[6] = new Pack.AnimationMixer( model );
                 //同时将这个外部模型的动画全部绑定到动画混合器里面
-                mixer[0].clipAction(anima[0]).play();//b
-                mixer[1].clipAction(anima[3]).play();//d
-                mixer[2].clipAction(anima[1]).play();//a
+                mixer[0].clipAction(anima[5]).play();//b
+                mixer[1].clipAction(anima[7]).play();//d
+                mixer[2].clipAction(anima[3]).play();//a
                 mixer[3].clipAction(anima[6]).play();//e
-                mixer[4].clipAction(anima[7]).play();//u
-                mixer[5].clipAction(anima[10]).play();//static
-                static_animate();
+                mixer[4].clipAction(anima[10]).play();//u
+                mixer[5].clipAction(anima[1]).play();//static
+                mixer[6].clipAction(anima[0]).play();//static
             })
-        }
-        var static_animate = function (){//播放口型动画（function animate）
-            requestAnimationFrame( static_animate );
-            mixer[5].update(100)
         }
         window.play=play;
         function play() {
+            var boneArm=[]
+            var boneFace=[]
+            for(i=70;i<120;i++){//216
+                boneFace.push(i);
+            }
+            for(i=0;i<216;i++){//216
+                boneArm.push(i);
+            }
+            var stop=false;
             console.log("开始播放动画");
-            if(past_time < 0){
-                past_time = -1000;
-            } else past_time = 0;
+
             animate0();//开始播放动画
+            function animate0() {
+                requestAnimationFrame( animate0 );
+                var time = clock.getDelta() * 1000;
+                if(stop){
+                    mixer[6].update(0.01);//手臂
+                }else{
+                    upda = getUpda();
+                    mixer[5].update(0.01,boneArm);//肢体
+                    mixer[upda].update(0.019,boneFace);//口型
+                    past_time += time;
+                }
+            }
+            function getUpda(){
+                var n=parseInt( past_time / move_time );
+                console.log(move_num,n)
+                if(n<move_num){
+                    upda = order_list[0][n];
+                }else{
+                    upda = order_list[0][move_num-1]//播放过慢
+                }
+                return upda;
+            }
             mp3.play();
 
             mp3.addEventListener('ended', function () {//监听到播放结束后，在此处可调用自己的接口
                 console.log("完成语音播放！")
+                stop=true;
             }, false);
         }
-        function animate0() {
-            requestAnimationFrame( animate0 );
-            var time = clock.getDelta() * 1000;
-            past_time += time;
-            if(past_time > 0) {
-                upda = getUpda();
-                mixer[upda].update(time/1000);
-            }
-        }
-        function getUpda(){
-            var upda = 5;
-            var num = order_list.length;//order_list是动画播放序列
-            let i;
-            for(i = 0; i < num; i++){
-                if(past_time <time_line[i]) break;
-            }
-            var list_num = i-1;//当前在读哪句话
-            var move_list_now = order_list[list_num];//当前这句话的动作序列
-            var move_num = move_list_now.length;//当前动作序列的动作数量
-            var move_time = time_list[list_num]/move_num;//每个动作花费的时间
-            var p_time = past_time-time_line[list_num];//开始读这句话时经过的时间
-            //console.log(list_num, move_list_now, move_num, move_time, p_time);
-            if(p_time < time_list[list_num]){//当还在读句子时
-                upda = move_list_now[parseInt( p_time / move_time )];
-            }
-            return upda;
-        }
+
+
     }
     this.host0=function () {
         var loader= new THREE.GLTFLoader();
